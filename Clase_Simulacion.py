e8d149a8275dcf5be86b67c7d3715ca83d12a595
Felipe Rosental <Frosental@itba.edu.ar>
2023-07-05 18:02:00 -0300

    algunos metodos nuevos y anotaciones

--- Clase_Simulacion.py
+++ Clase_Simulacion.py
@@ -19,17 +19,20 @@ class Routing_Sim:
         """Input: Objeto simulacion, destino final del mensaje (Int), mensaje (String), origen del mensaje (Int)\n
         Funcion: Iniciar la simulacion del programa\n
         Output: Nada"""
-        for _ in range(origen, destino_final):
+        inicio = datetime.now()
+        for nodo in range(origen, destino_final):
             nuevo_paquete = mensaje
-            self.routers[origen].enviar_paquete(nuevo_paquete)
-            for router in self.routers:
+            self.routers[nodo].enviar_paquete(nuevo_paquete)
+            for router in self.routers: # Habría que hacer esto bien, con una lista enlazada
                 router.procesar_paquete
                 if router.estado == "AGREGADO" and router.posicion <= destino_final:
                     router.estado = "ACTIVO"
                     Router.actualizar_csv("ACTIVO", "System_log.csv", router.posicion)
                 elif router.posicion == destino_final:
                     Routing_Sim.ver_paquete(destino_final, mensaje, origen)
-                
+        fin = datetime.now()
+        tiempo_simulado = (fin-inicio).total_seconds() # Con esto habria que hacer que el tiempo de la simulacion sea el ingresado como parametro
+        
     @staticmethod
     def ver_paquete (destino, mensaje, origen):
         """Input: Destino del mensaje (Int), mensaje (String), origen del mensaje (Int)\n
